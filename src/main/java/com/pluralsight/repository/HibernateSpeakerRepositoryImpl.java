7d32e4f083fe7a7c559ab8856bf927e52b4e2cea
gcsecsey <gcsecsey@gmail.com>
2020-05-25 19:04:56 +0200

    7.3 FactoryBean

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+  @Autowired
+  private Calendar calendar;
+
   @Override
   public List<Speaker> findAll() {
     List<Speaker> speakers = new ArrayList<Speaker>();
@@ -16,6 +22,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
     speaker.setFirstName("John");
     speaker.setLastName("Doe");
+
+    System.out.println("cal " + calendar.getTime());
 
     speakers.add(speaker);
 
